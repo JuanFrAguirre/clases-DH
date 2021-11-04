64d313540628bada3654278c0d8bfb671c0b749d
Juan Francisco Aguirre <jf_aguirre@hotmail.com>
2021-11-04 10:13:45 -0300

    finished working with forms

--- app.js
+++ app.js
@@ -9,5 +9,8 @@ app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './views/index.html'))
 })
+app.get('/register', (req, res) => {
+  res.sendFile(path.join(__dirname, './views/register.html'))
+})
 
 app.listen(PORT, () => console.log('servidor escuchando en el puerto ' + PORT))
